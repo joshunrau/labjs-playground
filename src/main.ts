6d0420ca44c789c0a1ed55f7313730102a471f9a
joshunrau <contact@joshuaunrau.com>
2023-11-20 13:36:39 -0500

    tsconfig

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log('hello');
+import { Component } from 'lab.js/core';
+
+const component = new Component({
+  responses: {
+    'keypress(s)': 'left',
+    'keypress(l)': 'right'
+  },
+  timeout: 1000
+});
+
+await component.run();
